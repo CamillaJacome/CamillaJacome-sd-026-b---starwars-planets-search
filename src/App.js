e76a52d2cfe2150d6c4ec4975684f9e52359cb3a
Camilla Jácome <camilla.cstrc@gmail.com>
2023-01-27 10:08:09 -0300

    inicia testes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
+import ContextProvider from './context/ContextProvider';
 import Table from './Compoments/Table';
 
 function App() {
   return (
     <div>
-      <Table />
+      <ContextProvider>
+        <Table />
+      </ContextProvider>
     </div>
   );
 }
--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,9 +1,39 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, act } from '@testing-library/react';
 import App from '../App';
+import testData from '../../cypress/mocks/testData'
 
-test('I am your test', () => {
+const mock = () => {
+  jest.spyOn(global, 'fetch');
+  global.fetch.mockResolvedValueOnce({
+    json: jest.fn().mockResolvedValue(testData),
+  });
+};
+
+test('se a API foi chamada', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Hello, App!/i);
-  expect(linkElement).toBeInTheDocument();
+  jest.spyOn(global, 'fetch');
+  render(<App />);
+  // verifica a chamada da api
+  expect(fetch).toHaveBeenCalledTimes(1);
+  expect(fetch).toHaveBeenCalledWith('https://swapi.dev/api/planets');
+});
+test('se existe o input de filtro', () => { 
+  mock();
+  render(<App />);
+  const inputSearch = screen.getByTestId('name-filter');
+  expect(inputSearch).toBeInTheDocument();
+});
+test('se existe filtros para valores numéricos', () => {
+  mock();
+  render(<App />);
+  const inputDropdown = screen.getByTestId('column-filter');
+  const inputFaixaValor = screen.getByTestId('comparison-filter');
+  const inputValueFilter = screen.getByTestId('value-filter');
+  const buttonFilter = screen.getByTestId('button-filter');
+
+  expect(inputDropdown).toBeInTheDocument();
+  expect(inputFaixaValor).toBeInTheDocument();
+  expect(inputValueFilter).toBeInTheDocument();
+  expect(buttonFilter).toBeInTheDocument();
 });
